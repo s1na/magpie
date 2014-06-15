0adbd28aa317c192fd38535323d3c40dcfbdb934
Sina Mahmoodi <itz.s1na@gmail.com>
2014-06-15 14:54:50 +0430

    Add grid search for finding the optimal parameters of linear svc.

--- magpie/sklearn-solution.py
+++ magpie/sklearn-solution.py
@@ -4,18 +4,23 @@ from sklearn.feature_selection import SelectPercentile, chi2, f_classif
 from sklearn.cross_validation import train_test_split
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.svm import LinearSVC, SVC
+from sklearn.grid_search import GridSearchCV
 from sklearn import metrics
 
 from old_hamshahri_reader import OldHamshahriReader
 import config
 
+
+
+tuned_params = [{'C': [1, 10, 100, 1000]}]
 if __name__ == '__main__':
     rd = OldHamshahriReader(root=config.CORPORA_ROOT)
     docs, labels = rd.sklearn_docs(config.TOT_DOCS)
     #vectorizer = CountVectorizer(docs)
-    vectorizer = TfidfVectorizer()
+    vectorizer = TfidfVectorizer(lowercase=False, max_df=0.8)
 
     fs = vectorizer.fit_transform(docs)
+    #vectorizer.build_preprocessor()
     selector = SelectPercentile(chi2, percentile=10)
     selector.fit(fs, labels)
     fs = selector.transform(fs)
@@ -26,9 +31,10 @@ if __name__ == '__main__':
     clf = BernoulliNB()
     clf.fit(fs_train, labels_train)
     pred = clf.predict(fs_test)
-    clf2 = LinearSVC()
+    clf2 = GridSearchCV(LinearSVC(), tuned_params, cv=5, scoring='accuracy')
     clf2.fit(fs_train, labels_train)
     pred2 = clf2.predict(fs_test)
+
     #print metrics.classification_report(labels_test, pred)
     print "BernoulliNB ** Accuracy: %f\tPrecision: %f\tRecall: %f\tF1: %f" % (
         metrics.accuracy_score(labels_test, pred),
@@ -36,7 +42,9 @@ if __name__ == '__main__':
         metrics.recall_score(labels_test, pred),
         metrics.f1_score(labels_test, pred),
     )
+
     #print metrics.classification_report(labels_test, pred2)
+    print clf2.best_estimator_
     print "LinearSVC ** Accuracy: %f\tPrecision: %f\tRecall: %f\tF1: %f" % (
         metrics.accuracy_score(labels_test, pred2),
         metrics.precision_score(labels_test, pred2),
